4602937abbd7b63f8329df04e087b3b27562c128
Haning <100738360+Choiyuhyeon@users.noreply.github.com>
2024-05-12 23:36:23 +0900

    feat: 리소스, 터미널 컴포넌트 수정

--- web/react-app/src/pages/Train.js
+++ web/react-app/src/pages/Train.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './Train.css';
 import { PieChart, Pie, Cell } from 'recharts';
 
-const ExecuteCommand = () => {
+const ExecuteCommand = ({ endpoint, locationName }) => {
   const [input, setInput] = useState('');
   const [output, setOutput] = useState('');
   const [error, setError] = useState('');
@@ -14,14 +14,14 @@ const ExecuteCommand = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    // 터미널 서버 연동 부분 (211 line 정도 보면 연동관련 내용 나와있습니다~!)
     try {
-      const selectedCountry = e.target.querySelector('select').value; // 선택된 나라 값을 가져옴 (US, UK, KR).
-      const response = await fetch('/execute', {
+      const response = await fetch(endpoint, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ command: input, country: selectedCountry }) // 선택된 나라 값을 서버로 전송 (US, UK, KR)
+        body: JSON.stringify({ command: input })
       });
 
       if (!response.ok) {
@@ -38,129 +38,100 @@ const ExecuteCommand = () => {
     }
   };
 
-  // 터미널 컴포넌트 jsx 시작
   return (
-    <div className="terminal-container">
-      <div className="terminal-box">
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            value={input}
-            onChange={handleInputChange}
-            placeholder={'Please enter the command you want! This is real terminal'}
-            required
-            style={{
-              padding: '10px',
-              fontSize: '16px',
-              border: '2px solid #ccc',
-              borderRadius: '4px',
-              marginRight: '10px',
-              backgroundColor: 'black',
-              color: 'white',
-              width: '700px',
-              height: '300px',
-              textAlign: 'left',
-              fontFamily: 'monospace',
-              caretColor: 'white',
-              // Placeholder 스타일
-              '::placeholder': {
-                color: '#999', // Placeholder 텍스트 색상 변경
-                fontStyle: 'italic' // 이탤릭체로 변경
-              }
-            }}
-          />
-
-          {/* ssh선택 부분 */}
-          <select>
-            <option value="US">US</option>
-            <option value="UK">UK</option>
-            <option value="KR">KR</option>
-          </select>
-
-          <button type="submit" className="execute-btn">
-            Execute
-          </button>
-        </form>
-        {output && (
-          <div>
-            <h2>Output:</h2>
-            <pre>{output}</pre>
-          </div>
-        )}
-        {error && (
-          <div>
-            <h2>Error:</h2>
-            <pre>{error}</pre>
-          </div>
-        )}
+    <div className="re-te">
+      <div className="terminal-container">
+        <span className="resource-label">terminal ({locationName})</span>
+        <p></p>
+        <br></br>
+        <div className="top-charts">
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              value={input}
+              onChange={handleInputChange}
+              placeholder={'Please enter the command'}
+              required
+              style={{
+                padding: '10px',
+                fontSize: '16px',
+                border: '2px solid #ccc',
+                borderRadius: '4px',
+                marginRight: '10px',
+                backgroundColor: 'black',
+                color: 'white',
+                width: '300px',
+                height: '100px',
+                textAlign: 'left',
+                fontFamily: 'monospace',
+                caretColor: 'white',
+                '::placeholder': {
+                  color: '#999',
+                  fontStyle: 'italic'
+                }
+              }}
+            />
+            <button type="submit" className="execute-btn">
+              Execute
+            </button>
+          </form>
+          {output && (
+            <div>
+              <h2>Output:</h2>
+              <pre>{output}</pre>
+            </div>
+          )}
+          {error && (
+            <div>
+              <h2>Error:</h2>
+              <pre>{error}</pre>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
 };
 
 const Train = () => {
-  const [resource, setResource] = useState({
-    cpu: 30,
-    memory: 0,
-    gpu: 0,
-    epoch: 0
-  });
+  const [resourceUS, setResourceUS] = useState({ cpu: 0, memory: 0, gpu: 0, epoch: 0 });
+  const [resourceUK, setResourceUK] = useState({ cpu: 0, memory: 0, gpu: 0, epoch: 0 });
+  const [resourceKR, setResourceKR] = useState({ cpu: 0, memory: 0, gpu: 0, epoch: 0 });
 
-  const [output, setOutput] = useState('');
-
-  const fetchData = async () => {
+  const fetchData = async (endpoint, setter) => {
     try {
-      const response = await fetch('/get_resource');
+      const response = await fetch(endpoint);
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
       const data = await response.json();
-      setResource(data);
+      setter(data);
     } catch (error) {
       console.error('Error fetching data:', error);
     }
   };
 
+  // get_resourceUS이런식으로 요청했습니다!
   useEffect(() => {
-    fetchData();
+    fetchData('/get_resourceUS', setResourceUS);
+    fetchData('/get_resourceUK', setResourceUK);
+    fetchData('/get_resourceKR', setResourceKR);
   }, []);
 
-  const data = [
-    { name: 'CPU', value: resource.cpu },
-    { name: 'Memory', value: resource.memory },
-    { name: 'GPU', value: resource.gpu },
-    { name: 'Epoch', value: resource.epoch }
-  ];
+  const renderResourceTerminal = (resource, locationName) => {
+    const data = [
+      { name: 'CPU', value: resource.cpu },
+      { name: 'Memory', value: resource.memory },
+      { name: 'GPU', value: resource.gpu },
+      { name: 'Epoch', value: resource.epoch }
+    ];
 
-  const colors = ['#00FF00', '#FFFF00', '#FFA500', '#FF0000'];
+    const colors = ['#00FF00', '#FFFF00', '#FFA500', '#FF0000'];
 
-  return (
-    <div className="container">
-      <div className="info">
-        {/* 클라우드정보 */}
-        <div className="label1">
-          Cloud info
-          <p></p>
-          <span className="cloudinfo">AWS(example)</span>
-        </div>
-        {/* 지역 */}
-        <div className="label2">
-          Region
-          <p></p>
-          <span className="region">UK(example)</span>
-        </div>
-        {/* 현재 탄소 밀집도 */}
-        <div className="label3">
-          Current carbon density
-          <p></p>
-          <span className="carbon-density">example</span>
-        </div>
-      </div>
-
-      <div className="resource-terminal">
+    return (
+      <div className="re-te">
         <div className="resource-container">
-          <span className="resource-label">resources in use</span>
-          {/* 위 차트 시작 */}
+          <span className="resource-label">resources in use ({locationName})</span>
           <div className="top-charts">
             {data.slice(0, 2).map((entry, index) => (
               <div className="circular-progress" key={index}>
@@ -171,9 +142,9 @@ const Train = () => {
                     cy="50%"
                     startAngle={90}
                     endAngle={-270}
-                    innerRadius="50%"
-                    outerRadius="70%"
-                    fill={[colors[index], '#eee']} // 변경된 부분
+                    innerRadius="40%"
+                    outerRadius="60%"
+                    fill={[colors[index], '#eee']}
                     dataKey="value"
                     labelLine={false}
                   >
@@ -182,14 +153,13 @@ const Train = () => {
                   </Pie>
                 </PieChart>
                 <div className={`inner${index + 1}`}>
-                  <span className="resource-name">{entry.name}</span> {/* 리소스 이름 */}
-                  <br /> {/* 개행 */}
-                  <span className="resource-value">{entry.value}%</span> {/* 리소스 수치 */}
+                  <span className="resource-name">{entry.name}</span>
+                  <br />
+                  <span className="resource-value">{entry.value}%</span>
                 </div>
               </div>
             ))}
           </div>
-          {/* 아래 차트 시작 */}
           <div className="bottom-charts">
             {data.slice(2).map((entry, index) => (
               <div className="circular-progress" key={index}>
@@ -200,9 +170,9 @@ const Train = () => {
                     cy="50%"
                     startAngle={90}
                     endAngle={-270}
-                    innerRadius="50%"
-                    outerRadius="70%"
-                    fill={[colors[index + 2], '#eee']} // 변경된 부분
+                    innerRadius="40%"
+                    outerRadius="60%"
+                    fill={[colors[index + 2], '#eee']}
                     dataKey="value"
                     labelLine={false}
                   >
@@ -211,20 +181,48 @@ const Train = () => {
                   </Pie>
                 </PieChart>
                 <div className={`inner${index + 3}`}>
-                  <span className="resource-name">{entry.name}</span> {/* 리소스 이름 */}
-                  <br /> {/* 개행 */}
-                  <span className="resource-value">{entry.value}%</span> {/* 리소스 수치 */}
+                  <span className="resource-name">{entry.name}</span>
+                  <br />
+                  <span className="resource-value">{entry.value}%</span>
                 </div>
               </div>
             ))}
           </div>
         </div>
-        {/* 결과 출력 */}
-        <div className="output">
-          <pre>{output}</pre>
+      </div>
+    );
+  };
+
+  return (
+    <div className="container">
+      <div className="info">
+        <div className="label1">
+          Cloud info :<p></p>
+          <span className="cloudinfo">AWS(example)</span>
         </div>
-        {/* ExecuteCommand 터미널 컴포넌트 추가 */}
-        <ExecuteCommand />
+        <div className="label2">
+          Region :<p></p>
+          <span className="region">UK(example)</span>
+        </div>
+        <div className="label3">
+          Current carbon density :<p></p>
+          <span className="carbon-density">example</span>
+        </div>
+      </div>
+
+      <div className="resource-terminal">
+        <div className="re-te">
+          {renderResourceTerminal(resourceUS, 'US')}
+          <ExecuteCommand endpoint="/sshUS" locationName="US" />
+        </div>
+        <div className="re-te">
+          {renderResourceTerminal(resourceUK, 'UK')}
+          <ExecuteCommand endpoint="/sshUK" locationName="UK" />
+        </div>
+        <div className="re-te">
+          {renderResourceTerminal(resourceKR, 'KR')}
+          <ExecuteCommand endpoint="/sshKR" locationName="KR" />
+        </div>
       </div>
     </div>
   );
